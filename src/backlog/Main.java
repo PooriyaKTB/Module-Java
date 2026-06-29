bdf51e819c0fd9611cd58cebf75c645a9ebf293d
Pooriya <pooriya.ketabi@gmail.com>
2026-06-29 23:05:54 +0100

    feat(backlog): Reverse Numbers Program

---
+++ src/backlog/Main.java
@@ -0,0 +1,27 @@
+package src.backlog;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = reverseNumber(3956);
+
+        System.out.println(result);
+    }
+
+    /**
+     * @param number  the integer to be reversed
+     * @return the reversed integer
+     */
+    private static int reverseNumber(int number){
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int lastDigit = number % 10;
+
+            reversedNumber = (reversedNumber * 10) + lastDigit;
+
+            number /= 10;
+        }
+
+        return reversedNumber;
+    }
+}
