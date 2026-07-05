10bff3e15580519e378a4b80ef18edb14bdc2ff4
Pooriya <pooriya.ketabi@gmail.com>
2026-07-05 20:17:17 +0100

    style: Format code and fix Javadoc

--- src/backlog/Main.java
+++ src/backlog/Main.java
@@ -8,17 +8,16 @@ public class Main {
     }
 
     /**
-     * @param number  the integer to be reversed
+     * This method Reverses the given integer.
+     * @param number the integer to be reversed
      * @return the reversed integer
      */
-    private static int reverseNumber(int number){
+    private static int reverseNumber(int number) {
         int reversedNumber = 0;
 
         while (number != 0) {
             int lastDigit = number % 10;
-
             reversedNumber = (reversedNumber * 10) + lastDigit;
-
             number /= 10;
         }
 
