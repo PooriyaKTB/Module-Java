bf899d1bbd500d387cc582daceae45520a5954ca
Pooriya <pooriya.ketabi@gmail.com>
2026-07-05 19:52:21 +0100

    feat(prep): Javadocs updated

--- src/backlog/Main.java
+++ src/backlog/Main.java
@@ -1,16 +1,17 @@
 package src.backlog;
 
-public class Main {
+public class /**/Main {
     public static void main(String[] args) {
         printPrimeNum(1000);
     }
 
     /**
+     * This method prints every prime numbers less than or equal to the number that passed as argument
      * @param limitWithin The limit that we want to check for primes within
      */
-    private static void printPrimeNum(int limitWithin){
-        for(int i=2; i<=limitWithin; i++){
-            if (PrimeChecker.isPrimeNumber(i)){
+    private static void printPrimeNum(int limitWithin) {
+        for (int i = 2; i <= limitWithin; i++) {
+            if (PrimeChecker.isPrimeNumber(i)) {
                 System.out.print(i + " ");
             }
         }
