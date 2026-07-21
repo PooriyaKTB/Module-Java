cbd0eb56182e1e331eadd8d4619cc61a4597fc14
Pooriya <pooriya.ketabi@gmail.com>
2026-07-21 01:18:13 +0100

    feat(prep): Sprint 3 - exercise 1.3 Stretch Goal

--- src/prep/TestStringUtilities.java
+++ src/prep/TestStringUtilities.java
@@ -1,6 +1,11 @@
 package src.prep;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -27,6 +32,7 @@ class TestStringUtilities {
                 () -> assertTrue(stringUtilities.isPalindrome(" ")),
                 () -> assertTrue(stringUtilities.isPalindrome("")),
 
+                () -> assertFalse(stringUtilities.isPalindrome("lev el")),
                 () -> assertFalse(stringUtilities.isPalindrome("java")),
                 () -> assertFalse(stringUtilities.isPalindrome("Junit")),
                 () -> assertFalse(stringUtilities.isPalindrome("spring")),
@@ -56,5 +62,75 @@ class TestStringUtilities {
                 () -> assertEquals(List.of(""), stringUtilities.splitCommaSeparatedLine("")),
                 () -> assertThrows(IllegalArgumentException.class, () -> stringUtilities.splitCommaSeparatedLine(null))
         );
+    }
+
+
+//  *** Stretch Goal - Parameterised tests ***
+
+//  ** @ParameterizedTest with @ValueSource **
+    @ParameterizedTest(name = "\"{0}\" is palindrome.")
+    @ValueSource(strings = {"level", "Radar", "wow", "121", "P", " ", ""})
+    @DisplayName("@ParameterizedTest for isPalindrome() using @ValueSource (expect true)")
+    void isPalindromeVS(String string) {
+        assertTrue(stringUtilities.isPalindrome(string));
+    }
+
+    @ParameterizedTest(name = "\"{0}\" is not palindrome.")
+    @ValueSource(strings = {"lev el","java", "Junit", "spring", "1212"})
+    @DisplayName("@ParameterizedTest for isPalindrome() using @ValueSource (expect false)")
+    void isNotPalindromeVS(String string) {
+        assertFalse(stringUtilities.isPalindrome(string));
+    }
+
+//  ** @ParameterizedTest with @CsvSource **
+    @ParameterizedTest(name = "Test {index}: is \"{0}\" Palindrome? {1}")
+    @CsvSource({
+            "level,true",
+            "Radar,true",
+            "wow,true",
+            "121,true",
+            "P,true",
+            "' ',true",
+            "'',true",
+            "lev el,false",
+            "java,false",
+            "Junit,false",
+            "spring,false",
+            "1212,false"
+    })
+    @DisplayName("@ParameterizedTest for isPalindrome() using @CsvSource")
+    void isPalindromeCsv(String input, boolean expected) {
+        assertEquals(expected, stringUtilities.isPalindrome(input));
+    }
+
+//  ** @ParameterizedTest with @MethodSource **
+    @ParameterizedTest(name = "Is \"{0}\" Palindrome? {1}")
+    @MethodSource("palindromeWords")
+    @DisplayName("@ParameterizedTest for isPalindrome() using @MethodSource")
+    void isPalindromeMs(String word, boolean expected) {
+        assertEquals(expected, stringUtilities.isPalindrome(word));
+    }
+
+    private static List<Arguments> palindromeWords() {
+        return List.of(
+                Arguments.of("level", true),
+                Arguments.of("Radar", true),
+                Arguments.of("wow", true),
+                Arguments.of("121", true),
+                Arguments.of("P", true),
+                Arguments.of(" ", true),
+                Arguments.of("", true),
+                Arguments.of("java", false),
+                Arguments.of("Junit", false),
+                Arguments.of("spring", false),
+                Arguments.of("1212", false)
+        );
+    }
+
+//  ** Test for isPalindrome() with null input (must come within each of the approaches chosen above) **
+    @Test
+    @DisplayName("Test for isPalindrome() with null input")
+    void testIsPalindromeNullThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> stringUtilities.isPalindrome(null));
     }
 }
