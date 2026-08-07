a51a6863743b9c59ddcec5c1892a0a68ccba433a
Pooriya <pooriya.ketabi@gmail.com>
2026-08-07 22:23:15 +0100

    feat(prep): Exercise 3.2

--- src/main/java/exercise3x/FileReaderService.java
+++ src/main/java/exercise3x/FileReaderService.java
@@ -6,15 +6,13 @@ public class FileReaderService {
 
     public String readFile(String fileName) throws InvalidFileFormatException {
 
-        BufferedReader reader = null;
         String content = "";
 
         if (fileName == null) {
             throw new IllegalArgumentException("Input can not be null!!");
         }
 
-        try {
-            reader = new BufferedReader(new FileReader(fileName));
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
 
             while ((line = reader.readLine()) != null) {
@@ -26,13 +24,6 @@ public class FileReaderService {
             System.out.println("File not found!");
         } catch (IOException e) {
             System.out.println("Reading file failed!");
-        } finally {
-            try {
-                if (reader != null)
-                    reader.close();
-            } catch (IOException e) {
-                System.out.println("Closing file failed!");
-            }
         }
         return content;
     }
